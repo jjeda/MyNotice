e9732b64f1141e39abc8cc9607dd35bf2a341d20
jjeda <jjeda@naver.com>
2019-06-18 15:09:34 +0900

    MyNotice Start

---
+++ src/main/java/me/jjeda/mynotice/MynoticeApplication.java
@@ -0,0 +1,13 @@
+package me.jjeda.mynotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynoticeApplication.class, args);
+	}
+
+}
