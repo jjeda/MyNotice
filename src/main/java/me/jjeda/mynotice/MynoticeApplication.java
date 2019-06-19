acee06f9b81a39dc5aac6f868b18779ece2300b0
jjeda <jjeda@naver.com>
2019-06-19 14:10:41 +0900

    기본 게시판 완성

--- src/main/java/me/jjeda/mynotice/MynoticeApplication.java
+++ src/main/java/me/jjeda/mynotice/MynoticeApplication.java
@@ -1,7 +1,17 @@
 package me.jjeda.mynotice;
 
+import me.jjeda.mynotice.board.Board;
+import me.jjeda.mynotice.board.BoardRepository;
+import me.jjeda.mynotice.board.BoardType;
+import me.jjeda.mynotice.user.User;
+import me.jjeda.mynotice.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class MynoticeApplication {
@@ -10,4 +20,25 @@ public class MynoticeApplication {
 		SpringApplication.run(MynoticeApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception{
+		return(args -> {
+			User user = userRepository.save(User.builder()
+					.name("jjeda")
+					.password("test")
+					.email("jjeda@naver.com")
+					.createdDate(LocalDateTime.now())
+					.build());
+
+			IntStream.rangeClosed(1,200).forEach(index ->boardRepository.save(Board.builder()
+					.title("게시글"+index)
+					.subTitle("순서"+index)
+					.content("콘텐츠")
+					.boardType(BoardType.free)
+					.createdDate(LocalDateTime.now())
+					.updatedDate(LocalDateTime.now())
+					.user(user).build())
+			);
+		});
+	}
 }
--- src/test/java/me/jjeda/mynotice/JpaMappingTest.java
+++ src/test/java/me/jjeda/mynotice/JpaMappingTest.java
@@ -58,6 +58,9 @@ public class JpaMappingTest {
         assertThat(user.getEmail(), is(email));
 
         Board board = boardRepository.findByUser(user);
+        assertThat(board.getTitle(), is(boardTestTitle));
+        assertThat(board.getSubTitle(), is("서브 타이틀"));
+        assertThat(board.getContent(), is("콘텐츠"));
 
     }
 
